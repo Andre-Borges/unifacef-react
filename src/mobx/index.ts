d3c6de71c6704d7549ddb2ea24d4a585e9fba76a
André Borges <andre.borges@irroba.com.br>
2020-06-20 16:08:09 -0300

    Add Cache

--- src/mobx/index.ts
+++ src/mobx/index.ts
@@ -1,7 +1,8 @@
-import { router } from './router.store';
 import { home } from '../containers/home/store';
 import { combustivel } from '../containers/combustivel/store';
+import { router } from './router.store';
 import { starWars } from '../containers/star-wars/store';
 import { starWarsDetails } from '../containers/star-wars-details/store';
+import { cache } from '../containers/cache/store';
 
-export { router, home, combustivel, starWars, starWarsDetails };
+export { router, combustivel, home, starWars, starWarsDetails, cache };
