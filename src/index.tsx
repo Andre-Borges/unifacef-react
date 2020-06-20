361ab066445695e9f12ff419735aac0b140d65d6
André Borges <andre.borges@irroba.com.br>
2020-06-20 13:45:43 -0300

    Update

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
+import './plugins/sentry.plugin';
 
 import * as serviceWorker from './serviceWorker';
 import * as store from './mobx';
@@ -10,9 +11,8 @@ import ReactDOM from 'react-dom';
 import { Router } from 'react-router-dom';
 import Routes from './routes';
 import { createBrowserHistory } from 'history';
-import { router } from './mobx';
 import { syncHistoryWithStore } from 'mobx-react-router';
-import './plugins/sentry.plugin';
+import { router } from './mobx';
 import Loading from './components/loading';
 import './apis/axios.api';
 
@@ -35,4 +35,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
